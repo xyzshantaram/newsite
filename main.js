dc73e971888abd58e7f0df999458071c51e8b35e
Siddharth Singh <simsid66@gmail.com>
2021-07-07 20:32:05 +0530

    Remove touch event redirection

--- main.js
+++ main.js
@@ -51,6 +51,7 @@ function init() {
 
     window.onresize = function() {
         window.bubbles = new Bubbles(canvas, ctx, window.bubbles.entities);
+
     }
 }
 
@@ -237,30 +238,4 @@ class Bubbles {
             bubble.draw(this.ctx);
         }
     }
-}
-
-function touchHandler(event) {
-    var touches = event.changedTouches,
-        first = touches[0],
-        type = "mousemove";
-
-    // initMouseEvent(type, canBubble, cancelable, view, clickCount, 
-    //                screenX, screenY, clientX, clientY, ctrlKey, 
-    //                altKey, shiftKey, metaKey, button, relatedTarget);
-
-    var simulatedEvent = document.createEvent("MouseEvent");
-    simulatedEvent.initMouseEvent(type, true, true, window, 1,
-        first.screenX, first.screenY,
-        first.clientX, first.clientY, false,
-        false, false, false, 0 /*left*/ , null);
-
-    first.target.dispatchEvent(simulatedEvent);
-    if (event.type == 'touchmove')
-        event.preventDefault();
-}
-
-function touchInit() {
-    document.addEventListener("touchstart", touchHandler, true);
-    document.addEventListener("touchmove", touchHandler, true);
-    document.addEventListener("touchend", touchHandler, true);
 }
